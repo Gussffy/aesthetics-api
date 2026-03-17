88824cd3628b8bab98da52888129623816bd5308
Gussffy <gsf34062@gmail.com>
2026-03-17 20:06:43 -0300

    initialize Spring Boot project with Maven setup and basic application structure

---
+++ src/main/java/com/gustavo/aestheticsapi/AestheticsApiApplication.java
@@ -0,0 +1,13 @@
+package com.gustavo.aestheticsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AestheticsApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AestheticsApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gustavo/aestheticsapi/BarbeariaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gustavo.aestheticsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarbeariaApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
